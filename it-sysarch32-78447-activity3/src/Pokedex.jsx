e53bb89d798f7516778aad52f6258f476960740a
Dominic Restauro <78447-22214258@SERVER2.DOMAIN>
2024-03-16 10:23:14 +0800

    Pokemon Version 3

--- it-sysarch32-78447-activity3/src/Pokedex.jsx
+++ it-sysarch32-78447-activity3/src/Pokedex.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function Pokedex() {
   const [pokemonDetails, setPokemonDetails] = useState([]);
+  const [language, setLanguage] = useState('english');
 
   useEffect(() => {
     fetch('https://us-central1-it-sysarch32.cloudfunctions.net/pokemon')
@@ -9,14 +10,31 @@ function Pokedex() {
       .then(data => setPokemonDetails(data));
   }, []);
 
+  const handleLanguageChange = (newLanguage) => {
+    setLanguage(newLanguage);
+  };
+
   return (
     <div>
+      <div className="language-buttons">
+        <button onClick={() => handleLanguageChange('english')}>English</button>
+        <button onClick={() => handleLanguageChange('japanese')}>Japanese</button>
+        <button onClick={() => handleLanguageChange('chinese')}>Chinese</button>
+        <button onClick={() => handleLanguageChange('french')}>French</button>
+      </div>
       {pokemonDetails.map((pokemon, index) => (
         <div className="cardpokemon" key={index}>
           <img className="pic" alt="Pokemon Picture" src={pokemon.image} />
           <h2 className="id">{pokemon.id}</h2>
-          <h2 className="name">{pokemon.name.english}</h2>
-          <h2 className="type">{pokemon.type.join(", ")}</h2>
+          <h2 className="name">{pokemon.name[language]}</h2>
+          {pokemon.type.length === 1 ? (
+            <h2 className="type">Type: {pokemon.type[0]}</h2>
+          ) : (
+            <>
+              <h2 className="type">{pokemon.type[0]}</h2>
+              <h2 className="type2"> {pokemon.type[1]}</h2>
+            </>
+          )}
           <h3 className="base">
             HP: {pokemon.base.HP}, Attack: {pokemon.base.Attack}, Defense: {pokemon.base.Defense}, 
             Sp. Attack: {pokemon.base.Sp_Attack}, Sp. Defense: {pokemon.base.Sp_Defense}, Speed: {pokemon.base.Speed}
